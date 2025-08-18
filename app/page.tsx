7ded9363cbbfb7d3d132184442b790bb36d6ab37
Rohan Munot <rohanmunot24@gmail.com>
2025-08-18 19:41:22 +0530

    Add Shiny Button component and update registry

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 "use client";
-import { motion } from "motion/react";
+import { ShinyButton } from "@/registry/button/shiny-button/shiny-button";
+import { ArrowRightIcon } from "lucide-react";
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-4xl font-semibold">
-      <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        inset ui
-      </motion.div>
+    <div className="flex flex-col items-center justify-center h-screen text-4xl font-semibold gap-4">
+      <ShinyButton>
+        Button
+        <ArrowRightIcon />
+      </ShinyButton>
     </div>
   );
 }
