284f5002dc65873d40684835f719f8cf9ce2c4fd
Rohan Munot <rohanmunot24@gmail.com>
2025-09-25 00:45:19 +0530

    like save card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 "use client";
-import CheckboxLabel from "@/components/checkbox-label/checkbox-label";
-import { CheckboxComponent } from "@/components/checkbox/01_checkbox";
+
+import LikeSaveCard from "@/components/card/like-save-card.tsx/like-save-card";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-4xl font-semibold gap-4">
-      <div className="w-max flex flex-col gap-4">
-        <CheckboxComponent />
-        <CheckboxLabel id="terms" text="Strike Through" variants="strike" />
-        <CheckboxLabel id="listen" text="Listen to GYBE" />
-      </div>
+      <LikeSaveCard />
     </div>
   );
 }
