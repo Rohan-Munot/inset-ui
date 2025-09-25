b6ac64b4b38bf8a03a960b3cae3b7fa081ffa2aa
Rohan Munot <rohanmunot24@gmail.com>
2025-09-25 16:43:42 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,15 @@ import { initialTaskList } from "@/lib/pin-list-data";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-4xl bg-accent font-semibold gap-4">
-      <LikeSaveCard />
-      {/* <div
+      {/* <LikeSaveCard /> */}
+      <div
         className={cn(
           "flex w-full max-w-xl flex-col gap-6 rounded-3xl bg-card p-6 text-card-foreground h-[90%]",
           "shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(48,48,55,0.05),0_2px_3px_rgba(0,0,0,0.05)]"
         )}
       >
         <PinList taskList={initialTaskList} />
-      </div> */}
+      </div>
     </div>
   );
 }
--- components/card/like-save-card.tsx/like-save-card.tsx
+++ components/card/like-save-card.tsx/like-save-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
-import { HeartIcon } from "./heart-icon";
-import { BookmarkIcon } from "./save-icon";
+import { HeartIcon } from "../../icons/heart-icon";
+import { BookmarkIcon } from "../../icons/save-icon";
 import { cn } from "@/lib/utils";
 
 const LikeSaveCard = () => {
